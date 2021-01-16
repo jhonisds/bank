a8d70da742b8915ce1915dfaba1a42a987c8f950
Jhoni Santos <jhoni.sousa@fatec.sp.gov.br>
2021-01-15 22:33:11 -0300

    Feat: generate migrations

---
+++ priv/repo/migrations/20210116013038_accounts.exs
@@ -0,0 +1,16 @@
+defmodule Bank.Repo.Migrations.Accounts do
+  use Ecto.Migration
+
+  def change do
+    create table(:accounts) do
+      add :account_owner, :string
+      add :balance, :string
+      add :currency, :money_with_currency
+
+      timestamps()
+
+    end
+
+    create unique_index(:accounts, [:account_owner])
+  end
+end
