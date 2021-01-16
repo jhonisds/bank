5034eaef3bd74542736588e28ec8a6a12165152d
Jhoni Santos <jhoni.sousa@fatec.sp.gov.br>
2021-01-15 22:36:52 -0300

    Feat: add seeds for populating the database

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,15 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+alias Bank.Accounts
+
+accounts = [
+  %{account_owner: "Tupac", currency: Money.new(:BRL, 0), balance: "R$ 0"},
+  %{account_owner: "Notorious", currency: Money.new(:BRL, 500), balance: "R$ 500,55"},
+  %{account_owner: "Lauryn", currency: Money.new(:USD, 100), balance: "U$ 100.99"},
+  %{account_owner: "Dre", currency: Money.new(:EUR, 200), balance: "€ 200.05"},
+  %{account_owner: "Snoop", currency: Money.new(:AUD, 300), balance: "AU$ 300,00"}
+]
+
+Enum.each(accounts, &Accounts.create_account/1)
