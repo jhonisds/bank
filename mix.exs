f46a0d7dc5d43776c5cf3d03668e793081e5242c
Jhoni Santos <jhoni.sousa@fatec.sp.gov.br>
2021-02-12 18:59:47 -0300

    Refactor: fix version elixir and postgres

--- mix.exs
+++ mix.exs
@@ -7,7 +7,7 @@ defmodule Bank.MixProject do
     [
       app: :bank,
       version: "0.1.0",
-      elixir: "~> 1.7",
+      elixir: "~> 1.11.2",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
